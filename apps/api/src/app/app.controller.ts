07ce1a72ddb0fdb422892ab1762533f93c34935f
Naushad Ali <naushad.ali@snyder.tech>
2022-01-21 18:46:57 +0500

    chore: build

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { Message } from '@learning-apps/api-interfaces';
 
 import { AppService } from './app.service';
 
+import ShipStation from 'shipstation-node';
+const shipstationI = new ShipStation({apiKey: 'c336cbc277cc487eba23545e121ac54b', apiSecret: 'b33401fb4c944cbbbe9480cce56d0191'});
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
 
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
   }
 
-  @Get('shipment')
+  @Post('shipment')
   async shipment(@Body() param: any) {
     const bodyString = JSON.stringify(param);
     const rawRequestText = `@Body()>>> ${bodyString} `;
@@ -22,5 +26,10 @@ export class AppController {
     console.debug('rawRequestText', rawRequestText);
 
     return { bodyString, rawRequestText };
+  }
+
+  @Get('orders')
+  async orders() {
+    return await shipstationI.shipments.getAll()
   }
 }
